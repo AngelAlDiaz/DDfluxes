97b277277abfe717bba6af6400414b4038b6d134
AngelAlDiaz <126770461+AngelAlDiaz@users.noreply.github.com>
2026-02-25 00:16:02 +0100

    Added derivative of Bernoulli's Gen. function and flux w.r.t. V in ddSG.py

--- ddSG.py
+++ ddSG.py
@@ -6,7 +6,7 @@ Created on Thu Mar 6 13:29:22 2025
 """
 import numpy as np;
 
-def BernouilliGenFunction(x):
+def BernouilliGenFunction(x, returnDerivative=False):
     """
     Computes the Bernouilli's generating function appearing in the classical
     Scharfetter-Gummel method.
@@ -15,16 +15,21 @@ def BernouilliGenFunction(x):
     ----------
     x : numpy.array(float)
         Dimensionless argument of the function.
-
+    returnDerivative : bool, OPTIONAL
+        Whether to return the derivative.
     Returns
     -------
     numpy.array(float)
         Array with the values of the Bernoulli's function.
     """
     result = np.ones_like(x, float);
-    auxMask = (np.abs(x) > 1e-10);
-    result[auxMask > 0] = x[auxMask > 0]/(np.exp(x[auxMask > 0]) - 1.0);
-    return result;
+    safeIndices = np.nonzero(np.abs(x) > 1e-10)[0];
+    result[safeIndices] = x[safeIndices]/(np.exp(x[safeIndices]) - 1.0);
+    if returnDerivative:
+        deriv = -0.5*np.ones_like(x, float);
+        deriv[safeIndices] = result[safeIndices]*(1.0/x[safeIndices] - np.exp(x[safeIndices]));
+        return result, deriv;
+    else: return result;
 #end def
 
 def calcDDFlux_SG(ek_, el_, zDVkl_, returnJacobian=False):
@@ -48,17 +53,25 @@ def calcDDFlux_SG(ek_, el_, zDVkl_, returnJacobian=False):
     Returns
     -------
         Numerical carrier flux along bond from k to l.
-    """
+    """        
     expk, expl = np.exp(ek_), np.exp(el_);
-    B_zDV, BzDV = BernouilliGenFunction(-zDVkl_), BernouilliGenFunction(zDVkl_);
+    if returnJacobian: B_zDV, dB_zDV, BzDV, dBzDV = BernouilliGenFunction(-zDVkl_, returnDerivative=True), BernouilliGenFunction(zDVkl_, returnDerivative=True);
+    else: B_zDV, BzDV = BernouilliGenFunction(-zDVkl_), BernouilliGenFunction(zDVkl_);
     j = -(B_zDV*expl - BzDV*expk);
         
     if returnJacobian:
         DcDphik, DcDphil = (-1)*expk, (-1)*expl;
+        # Derivatives w.r.t. quasi-Fermi potential 
         djdphik = BzDV*DcDphik;
         djdphil = (-1)*B_zDV*DcDphil;
-        return j.copy(), djdphik.copy(), djdphil.copy();
+        # Derivatives w.r.t. electrostatic potential
+        z = kwargs['chargeNumber'];
+        djdVk = -z*( dB_zDV*expl + (dBzDV + BzDV)*expk );
+        djdVl = z*( (dB_zDV + B_zDV)*expl + dBzDV*expk );
+
+        return j.copy(), djdphik.copy(), djdphil.copy(), djdVk.copy(), djdVl.copy();
     else:
         return j.copy();
     #end if
 #end def
+
